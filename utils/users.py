e55ea8fc2a663989dea645d4a555e64ddccbe1d9
Max Z <max.zlotskiy@gmail.com>
2017-12-26 00:15:57 -0500

    fixed some legacy code

--- utils/users.py
+++ utils/users.py
@@ -1,11 +1,25 @@
 import sqlite3 #enables control of an sqlite database
 
 #-------------------------------------------------------------
-db_name = "data/ourDB.db"
+db_name = "data/chillDB.db"
 
 #Run once, creates the table
 def create_table():
-    cursor.execute("CREATE TABLE users (username TEXT PRIMARY KEY, password TEXT, full_name TEXT, likes TEXT, contributions TEXT);")
+    db = sqlite3.connect(db_name)
+    cursor = db.cursor()
+    cursor.execute("CREATE TABLE users (username TEXT PRIMARY KEY, password TEXT, pfp TEXT, best INTEGER, best_img TEXT, worst INTEGER, worst_img TEXT);")
+    db.commit()
+    db.close()
+
+#Given a username and password, will return true if the two correspond, false otherwise
+def validate_login(uname, pword):
+    db = sqlite3.connect(db_name)
+    c = db.cursor()
+    users = c.execute("SELECT password FROM users WHERE username='%s';" % (uname,)).fetchone()
+    db.close()
+    if users == None:
+        return False
+    return users[0] == pword
 
 def user_exists(username):
     db = sqlite3.connect(db_name)
@@ -23,17 +37,18 @@ def get_users():
     db = sqlite3.connect(db_name)
     c = db.cursor()
     command = "SELECT username FROM users;"
-    return c.execute(command).fetchall()
+    users = c.execute(command).fetchall()
+    db.close()
+    return users
 
 #Adds a new record to the users table    
-def add_new_user(db, user, pw, name):
+def add_new_user(user, pw, pfp):
     db = sqlite3.connect(db_name)
     c = db.cursor()
-    command = "INSERT INTO users VALUES ('%s', '%s', '%s', '', '');"%(user,pw,name)
+    command = "INSERT INTO users VALUES ('%s', '%s', '%s', 0, '', 0, '');"%(user,pw,pfp)
     c.execute(command)
     db.commit()
     db.close()
-    print 'done'
 
 #Updates a user's username to a new one    
 def update_username(old_user, new_user):
