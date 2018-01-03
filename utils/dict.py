5930ea3663f609efe2960b46fb0fde277a255ad4
soe43 <soe433@gmail.com>
2018-01-03 14:52:21 -0500

    Added start of dict api support

---
+++ utils/dict.py
@@ -0,0 +1,20 @@
+#install requests from http://docs.python-requests.org/en/master/user/install/#install
+import requests
+import json
+
+#CHANGE API_KEY TO "NOT SECURE" BEFORE SENDING TO GITHUB!!
+app_id = '4cbe4b04'
+app_key = '0ee540bf49e15d9f38e393e9ee97982f'
+
+#settings
+
+
+#requests
+url = 'https://od-api.oxforddictionaries.com/api/v1/wordlist/en/registers=Rare;'
+
+r = requests.get
+
+
+
+
+
