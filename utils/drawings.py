f0266c32050ac269070991eee14d80137885bdf8
soe43 <soe433@gmail.com>
2018-01-22 17:15:54 -0500

    testing get_images_by

--- utils/drawings.py
+++ utils/drawings.py
@@ -20,6 +20,37 @@ def add_drawing(username, encoded_image, word):
     db.commit()
     db.close()
 
+#Returns a dictionary with the following keys: image (encoded), word (what it is), artist (user who made it), id, solved (boolean) and guesses (a list of dictionaries; each dictionary has the keys username, guess, when and are sorted from earliest to last)
+def get_image(id):
+    if id == None:
+        return {"image": "/static/missing.png", "word": "", "score": 0, "artist": "", "id": None, "guesses":[]}
+    id = int(id)
+    db = sqlite3.connect(db_name)
+    c = db.cursor()
+    image_stats = c.execute("SELECT id, username, image, word, solved FROM drawings WHERE id = %d;" % id).fetchone()
+    wrong_guesses = c.execute("SELECT username, guess, timestamp FROM guesses WHERE drawing_id = %d ORDER BY timestamp ASC;" % id).fetchall()
+    db.close()
+    if image_stats == None:
+        return {}
+    else:
+        image_dict = tuple_to_dictionary(image_stats, ["id", "artist", "image", "word", "solved"])
+        image_dict["solved"] = (image_dict["solved"] == 1)
+        image_dict["guesses"] = []
+        for g in wrong_guesses:
+            image_dict["guesses"].append(tuple_to_dictionary(g, ["username", "guess", "when"]))
+        return image_dict
+
+#Returns a list of drawings that the specified user hasnt guessed yet.
+def random_drawings(username, count):
+    db = sqlite3.connect(db_name)
+    c = db.cursor()
+    list_id = c.execute("SELECT id FROM drawings WHERE username != '%s' AND solved = 0 AND id NOT IN (SELECT drawing_id FROM guesses WHERE username = '%s')" % (username, username)).fetchall()[:count]
+    db.close()
+    list_drawings = []
+    for id in list_id:
+        list_drawings.append(get_image(id[0]))
+    return list_drawings
+    
 #returns list of dictionaries containing images drawn by a specified user
 def get_images_by(username):
     #list_id = [1, 2, 3, 4]
